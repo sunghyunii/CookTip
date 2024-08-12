e56bb17c9dc87ae1c7e4d417882d1bf28ae28c41
COM <gauss0305@naver.com>
2024-08-12 14:42:54 +0900

    재료

---
+++ src/main/java/com/example/demo/repository/IngredientsRepository.java
@@ -0,0 +1,8 @@
+package com.example.demo.repository;
+
+import com.example.demo.entity.Ingredients;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+
+public interface IngredientsRepository extends JpaRepository<Ingredients,Long>, QuerydslPredicateExecutor<Ingredients> {
+}
