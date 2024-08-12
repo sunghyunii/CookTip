2b2aaca66c435dd4f73fbe0fb91a2fbb61fa39e9
COM <gauss0305@naver.com>
2024-08-12 17:23:58 +0900

    재료

---
+++ src/main/java/com/example/demo/service/IngredientsService.java
@@ -0,0 +1,19 @@
+package com.example.demo.service;
+
+import com.example.demo.dto.IngredientsDTO;
+import com.example.demo.entity.Ingredients;
+import org.springframework.stereotype.Service;
+
+@Service
+
+public class IngredientsService implements IngredientsServiceInt{
+
+    //재료 정보 조회
+
+    //재료 정보 등록
+
+    //재료 정보 수정
+
+    //재료 정보 삭제
+
+}
---
+++ src/main/java/com/example/demo/service/IngredientsServiceInt.java
@@ -0,0 +1,10 @@
+package com.example.demo.service;
+
+import com.example.demo.dto.IngredientsDTO;
+import com.example.demo.entity.Ingredients;
+
+public interface IngredientsServiceInt {
+    IngredientsDTO ingredientsDTO();
+    Ingredients ingredients();
+
+}
